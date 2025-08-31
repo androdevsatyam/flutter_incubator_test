0d29971766b8769d9e430a69af58ce74e21af608
SatyamArrest <136549599+SatyamArrest@users.noreply.github.com>
2025-08-31 22:37:31 +0530

    For Incubator Test Series 2nd Task Implement an extension on [String], parsing links from a text

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Flutter Incubator Test
 
+#### Run and test on Web
+
 ## Getting Started
 
 This project is a starting point my incubator test series
@@ -10,10 +12,12 @@ This project is a starting point my incubator test series
 
 #### 1.1 Mixins
 
-*   **Task 1: Extension Methods**
+*   **Task 1: Implement an extension on [DateTime], returning a [String] in format of**
     *   Explore how extension methods can add functionality to existing classes without modifying their source code.
     *   File: [`Task1.dart`](lib/1_dart_concepts/1_1_mixins/task_1.dart)
-*   **Task 2:** (To be defined)
+*   **Task 2:** (Implement an extension on [String], parsing links from a text)
+    *   Extension Method on String that parse string and identifies links in string 
+    *   File: [`Task2.dart`](lib/1_dart_concepts/1_1_mixins/task_2.dart)
 *   **Task 3:** (To be defined)
 
 ### Future Chapters
--- lib/1_dart_concepts/1_1_mixins/task_2.dart
+++ lib/1_dart_concepts/1_1_mixins/task_2.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:intl/intl.dart';
+
+import '../../main.dart';
 
 class Task2 extends StatefulWidget {
   const Task2({super.key});
@@ -8,8 +12,64 @@ class Task2 extends StatefulWidget {
 }
 
 class _Task2State extends State<Task2> {
+  List<InlineSpan> parsedText = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    TextEditingController textEditingController = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Task 2"),
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: textEditingController,
+                decoration: InputDecoration(
+                  hintText: "Type String with , separate",
+                ),
+              ),
+              needSpace(150, "h"),
+              RichText(text: TextSpan(children: parsedText)),
+              needSpace(150, "h"),
+              createInkWell(customTag("Parsing"), () {
+                parsedText =
+                    textEditingController.text.toString().parsingLinks();
+                setState(() {});
+              }),
+              needSpace(20, "h"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+extension Task2ExtensionMethod on String {
+  List<InlineSpan> parsingLinks() {
+    List<String> chunks = this.split(",");
+    List<InlineSpan> parsedList = [];
+    final RegExp linkRegExp = RegExp(
+      r'(https?:\/\/[^\s]+|[a-zA-Z0-9-]+\.[a-zA-Z]{2,})',
+      caseSensitive: false,
+    );
+    chunks.forEach((action) {
+      if (linkRegExp.hasMatch(action)) {
+        parsedList.add(
+          TextSpan(text: action, style: TextStyle(color: Colors.blue)),
+        );
+      } else {
+        parsedList.add(
+          TextSpan(text: action, style: TextStyle(color: Colors.black)),
+        );
+      }
+    });
+    return parsedList;
   }
 }
